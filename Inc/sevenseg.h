dc541e96f11a29e604e7d9e6787dbc7f1204ad88
bartoszp1992 <bartosz.pracz.92@gmail.com>
2019-11-07 14:27:10 +0100

    proto

--- Inc/sevenseg.h
+++ Inc/sevenseg.h
@@ -23,15 +23,15 @@
 #define SG_OFF HAL_GPIO_WritePin(SG_GPIO_Port, SG_Pin, 0)
 
 //digits
-#define D1_ON HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, 1)
-#define D2_ON HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, 1)
-#define D3_ON HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 1)
-#define D4_ON HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 1)
+#define D1_ON HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, 0)
+#define D2_ON HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, 0)
+#define D3_ON HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 0)
+#define D4_ON HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 0)
 
-#define D1_OFF HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, 0)
-#define D2_OFF HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, 0)
-#define D3_OFF HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 0)
-#define D4_OFF HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 0)
+#define D1_OFF HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, 1)
+#define D2_OFF HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, 1)
+#define D3_OFF HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 1)
+#define D4_OFF HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 1)
 
 //turning off
 #define D_OFF D1_OFF; D2_OFF; D3_OFF; D4_OFF
@@ -40,11 +40,15 @@
 #define ALL_OFF D_OFF; S_OFF
 
 //display delay
-#define WAIT HAL_Delay(300)
+#define WAIT HAL_Delay(1)
 
 //buttons
-#define CLEAR HAL_GPIO_ReadPin(CLEAR_GPIO_Port, CLEAR_Pin) == 0
-#define SET HAL_GPIO_ReadPin(SET_GPIO_Port, SET_Pin) == 0
+#define UP HAL_GPIO_ReadPin(UP_GPIO_Port, UP_Pin) == 0
+#define LEFT HAL_GPIO_ReadPin(LEFT_GPIO_Port, LEFT_Pin) == 0
+#define ENTER HAL_GPIO_ReadPin(ENTER_GPIO_Port, ENTER_Pin) ==0
+#define RIGHT HAL_GPIO_ReadPin(RIGHT_GPIO_Port, RIGHT_Pin)==0
+#define DOWN HAL_GPIO_ReadPin(DOWN_GPIO_Port, DOWN_Pin)==0
+#define BACK HAL_GPIO_ReadPin(BACK_GPIO_Port, BACK_Pin)==0
 
 
 void display(int position, int number);
